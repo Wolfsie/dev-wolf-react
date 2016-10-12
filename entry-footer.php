7f1da8031f7c1e6ac552f93dc9a461b64c19c70c
Amanda Muir <muir.dev@gmail.com>
2016-10-11 21:05:28 -0400

    Revert "Correct Naming Error"
    This reverts commit 1fcac30a033d84222287f9fce478be7cd3fdaec8.

--- entry-footer.php
+++ entry-footer.php
@@ -1,7 +1,7 @@
 <footer class="entry-footer">
-<span class="cat-links"><?php _e( 'Categories: ', 'devWolfReact' ); ?><?php the_category( ', ' ); ?></span>
+<span class="cat-links"><?php _e( 'Categories: ', 'dev-wolf-react' ); ?><?php the_category( ', ' ); ?></span>
 <span class="tag-links"><?php the_tags(); ?></span>
 <?php if ( comments_open() ) { 
-echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'devWolfReact' ) ) . '</a></span>';
+echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'dev-wolf-react' ) ) . '</a></span>';
 } ?>
 </footer> 
--- functions.php
+++ functions.php
@@ -1,45 +1,45 @@
 <?php
-add_action( 'after_setup_theme', 'devWolfReact_setup' );
-function devWolfReact_setup()
+add_action( 'after_setup_theme', 'dev-wolf-react_setup' );
+function dev-wolf-react_setup()
 {
-load_theme_textdomain( 'devWolfReact', get_template_directory() . '/languages' );
+load_theme_textdomain( 'dev-wolf-react', get_template_directory() . '/languages' );
 add_theme_support( 'title-tag' );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
 global $content_width;
 if ( ! isset( $content_width ) ) $content_width = 640;
 register_nav_menus(
-array( 'main-menu' => __( 'Main Menu', 'devWolfReact' ) )
+array( 'main-menu' => __( 'Main Menu', 'dev-wolf-react' ) )
 );
 }
-add_action( 'wp_enqueue_scripts', 'devWolfReact_load_scripts' );
-function devWolfReact_load_scripts()
+add_action( 'wp_enqueue_scripts', 'dev-wolf-react_load_scripts' );
+function dev-wolf-react_load_scripts()
 {
 wp_enqueue_script( 'jquery' );
 }
-add_action( 'comment_form_before', 'devWolfReact_enqueue_comment_reply_script' );
-function devWolfReact_enqueue_comment_reply_script()
+add_action( 'comment_form_before', 'dev-wolf-react_enqueue_comment_reply_script' );
+function dev-wolf-react_enqueue_comment_reply_script()
 {
 if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
 }
-add_filter( 'the_title', 'devWolfReact_title' );
-function devWolfReact_title( $title ) {
+add_filter( 'the_title', 'dev-wolf-react_title' );
+function dev-wolf-react_title( $title ) {
 if ( $title == '' ) {
 return '&rarr;';
 } else {
 return $title;
 }
 }
-add_filter( 'wp_title', 'devWolfReact_filter_wp_title' );
-function devWolfReact_filter_wp_title( $title )
+add_filter( 'wp_title', 'dev-wolf-react_filter_wp_title' );
+function dev-wolf-react_filter_wp_title( $title )
 {
 return $title . esc_attr( get_bloginfo( 'name' ) );
 }
-add_action( 'widgets_init', 'devWolfReact_widgets_init' );
-function devWolfReact_widgets_init()
+add_action( 'widgets_init', 'dev-wolf-react_widgets_init' );
+function dev-wolf-react_widgets_init()
 {
 register_sidebar( array (
-'name' => __( 'Sidebar Widget Area', 'devWolfReact' ),
+'name' => __( 'Sidebar Widget Area', 'dev-wolf-react' ),
 'id' => 'primary-widget-area',
 'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 'after_widget' => "</li>",
@@ -47,15 +47,15 @@ register_sidebar( array (
 'after_title' => '</h3>',
 ) );
 }
-function devWolfReact_custom_pings( $comment )
+function dev-wolf-react_custom_pings( $comment )
 {
 $GLOBALS['comment'] = $comment;
 ?>
 <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
 <?php 
 }
-add_filter( 'get_comments_number', 'devWolfReact_comments_number' );
-function devWolfReact_comments_number( $count )
+add_filter( 'get_comments_number', 'dev-wolf-react_comments_number' );
+function dev-wolf-react_comments_number( $count )
 {
 if ( !is_admin() ) {
 global $id;
